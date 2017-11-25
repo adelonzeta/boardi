9f705c2a3b923dda0ab74dc4baac0f5b23f3a263
Adelon Zeta <adelonzeta@Adelons-MacBook-Air.local>
2017-11-25 13:53:00 -0800

    Update 2017-11-25T21:52:59.843Z

--- build/site/publish.js
+++
@@ -1,9 +0,0 @@
-var gulp    = require('gulp');
-var ghpages = require('gulp-gh-pages');
-
-module.exports = function() {
-  return gulp.src('dist/**/*')
-    .pipe(ghpages());
-};
-
-module.exports.dependencies = ['prod'];
